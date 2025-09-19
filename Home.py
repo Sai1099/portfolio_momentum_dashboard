a1de309da6380be6f88a020f028da9dd8d4de17d
sai murali <chsaimurali09@gmail.com>
2025-09-19 12:56:50 +0530

    holding

--- Home.py
+++ Home.py
@@ -117,7 +117,7 @@ kpi_df["Total Return"] = kpi_df["Total Return"].apply(lambda x: f"{x:.2%}")
 kpi_df["Max Drawdown"] = kpi_df["Max Drawdown"].apply(lambda x: f"{x:.2%}")
 kpi_df["Sharpe Ratio"] = kpi_df["Sharpe Ratio"].apply(lambda x: f"{x:.2f}")
 kpi_df = kpi_df.T
-st.dataframe(kpi_df,hide_index=True)
+st.dataframe(kpi_df)
 
 
 
